93d5b432b6a721a42eb2a4bd66c39662055410e8
KARTHICK ALAGAR <karthikarthick1949@gmail.com>
2024-08-20 18:06:37 +0530

    tst

---
+++ datatype.py
@@ -0,0 +1,24 @@
+....
+
+print()-print data on console/terminal
+type()-
+id()
+input()
+
+#type of casting function
+int()
+float()
+str()
+bool()
+complex()
+
+list()
+tuble()
+set()
+dict()
+
+bytes()
+bytearray()
+frozenset()
+None()
+....
---
+++ untitled2.py
@@ -0,0 +1,20 @@
+....
+
+print()-print data on console/terminal
+type()-
+id()
+input()
+
+#type of casting function
+int()
+float()
+str()
+bool()
+complex()
+list()
+tuble()
+set()
+dict()
+bytes()
+bytearray()
+frozenset()
